5ab9d9e31c5a8206e87eb7a517ce873e833cbeba
VladimirZhdanov <ConstHomespb@gmail.com>
2022-07-02 12:24:49 +0200

    some stuff

--- src/main/java/com/homel/leetcode/samples/numbers/Pow50.java
+++ src/main/java/com/homel/leetcode/samples/numbers/Pow50.java
@@ -2,16 +2,20 @@ package com.homel.leetcode.samples.numbers;
 
 public class Pow50 {
     public static void main(String[] args) {
-        System.out.println(pow(2, 5));
+        System.out.println(myPow(2, 5));
     }
 
-    public static double pow(double x, int n) {
-        if (n == 0)
-            return 1;
+    public static double myPow(double x, int n) {
+        if (n == 0) return 1;
+        if (n == Integer.MIN_VALUE) {
+            x = x * x;
+            n = n / 2;
+        }
         if (n < 0) {
             n = -n;
             x = 1 / x;
         }
-        return (n % 2 == 0) ? pow(x * x, n / 2) : x * pow(x * x, n / 2);
+
+        return (n % 2 == 0) ? myPow(x * x, n / 2) : x * myPow(x * x, n / 2);
     }
 }
